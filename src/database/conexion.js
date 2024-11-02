39b378a119f01b9742cc297e89a68f7e4e8567b8
james10munoz <ja6947775@gmail.com>
2024-11-02 00:33:27 -0500

    Actualizacion

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -11,17 +11,17 @@ export const pool = createPool({
 	// port: 3306,
 	// database: 'b9iytc2y2enbn9s1mwkk',
 
-	// host: '192.168.0.50',
-	// user: 'cesar',
-	// password: 'cesar',
-	// port: 3306,
-	// database: 'buddypet',
-
-	host: 'localhost',
-	user: 'root',
-	password: '',
+	host: '192.168.0.50',
+	user: 'cesar',
+	password: 'cesar',
 	port: 3306,
 	database: 'buddypet',
+
+	// host: 'localhost',
+	// user: 'root',
+	// password: '',
+	// port: 3306,
+	// database: 'buddypet',
 });
 
 
